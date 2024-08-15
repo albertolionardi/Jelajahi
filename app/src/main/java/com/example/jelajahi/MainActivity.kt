1028b2a6ef3ee54e5fdbc2af8197fb0f6b013ad4
Alberto <albertolionardi1@gmail.com>
2024-08-16 01:21:28 +0200

    Fix manifest, fix theme, add mountain page

--- app/src/main/java/com/example/jelajahi/MainActivity.kt
+++ app/src/main/java/com/example/jelajahi/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.example.jelajahi
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
+import android.content.Intent
+import android.widget.Button
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonToMountain = findViewById<Button>(R.id.buttonToMountain)
+        buttonToMountain.setOnClickListener {
+            val intent = Intent(this, MountainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
